a3f8f1acedd1ded1326cac79074e23a973e7f4e0
Daniel_ALD <daniellopezdib@gmail.com>
2024-11-07 04:44:04 -0500

    Modal fixes Most stable version

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,22 +1,31 @@
 // ProjectCard.js
 
 import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import './ProjectCard.css';
 
 const ProjectCard = ({ project }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // Open and close modal functions
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
   return (
     <>
-      <div className="project-card" onClick={openModal}>
-        <img src={project.image} alt={project.title} className="project-image" />
+      <motion.div
+        className="project-card"
+        onClick={openModal}
+        layoutId={`card-container-${project.id}`}
+        whileHover={{ scale: 1.05 }}
+      >
+        <motion.img
+          src={project.image}
+          alt={project.title}
+          className="project-image"
+          layoutId={`card-image-${project.id}`}
+        />
         <div className="project-overlay">
           <h3 className="project-title">{project.title}</h3>
-          <p className="project-description">{project.description.substring(0, 100)}...</p>
           {/* Technologies Tags */}
           {project.technologies && (
             <div className="project-technologies">
@@ -28,42 +37,60 @@ const ProjectCard = ({ project }) => {
             </div>
           )}
         </div>
-      </div>
+      </motion.div>
 
-      {/* Modal for detailed view */}
-      {isModalOpen && (
-        <div className="project-modal" onClick={closeModal}>
-          <div className="project-modal-content" onClick={(e) => e.stopPropagation()}>
-            <span className="project-modal-close" onClick={closeModal}>
-              &times;
-            </span>
-            <h2>{project.title}</h2>
-            <img src={project.image} alt={project.title} className="modal-image" />
-            <p>{project.description}</p>
-            {/* Technologies */}
-            {project.technologies && (
-              <div className="project-technologies">
-                {project.technologies.map((tech, index) => (
-                  <span key={index} className="project-tech-tag">
-                    {tech}
-                  </span>
-                ))}
+      <AnimatePresence>
+        {isModalOpen && (
+          <motion.div
+            className="project-modal"
+            onClick={closeModal}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <motion.div
+              className="project-modal-content"
+              onClick={(e) => e.stopPropagation()}
+              layoutId={`card-container-${project.id}`}
+            >
+              <span className="project-modal-close" onClick={closeModal}>
+                &times;
+              </span>
+              <motion.img
+                src={project.image}
+                alt={project.title}
+                className="modal-image"
+                layoutId={`card-image-${project.id}`}
+              />
+              <div className="modal-content-wrapper">
+                <h2 className="modal-title">{project.title}</h2>
+                <p className="project-description">{project.description}</p>
+                {/* Technologies Tags */}
+                {project.technologies && (
+                  <div className="project-technologies">
+                    {project.technologies.map((tech, index) => (
+                      <span key={index} className="project-tech-tag">
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
+                )}
+                {/* View Project Button */}
+                {project.link && (
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="view-project-button"
+                  >
+                    Visit Project
+                  </a>
+                )}
               </div>
-            )}
-            {/* View Project Button */}
-            {project.link && (
-              <a
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="view-project-button"
-              >
-                Visit Project
-              </a>
-            )}
-          </div>
-        </div>
-      )}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -1,3 +1,7 @@
+import project1Image from './Images/0.png';
+import project2Image from './Images/0.webp';
+
+
 export const categories = [
     {
       title: 'Featured Projects',
@@ -7,7 +11,7 @@ export const categories = [
           id: 1,
           title: 'Project One',
           description: 'Description for Project One.',
-          image: '/images/project1.jpg',
+          image: project1Image,
           link: 'https://example.com/project-one', // Add this line
           technologies: ['React', 'CSS', 'JavaScript'], // We'll use this later
         },
@@ -15,7 +19,7 @@ export const categories = [
           id: 2,
           title: 'Project Two',
           description: 'Description for Project Two.',
-          image: '/images/project2.jpg',
+          image:'0',
           link: 'https://example.com/project-one', // Add this line
           technologies: ['React', 'CSS', 'JavaScript'], // We'll use this later
         },
@@ -30,7 +34,7 @@ export const categories = [
           id: 101,
           title: 'Recent Project One',
           description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10',
-          image: '/images/recent1.jpg',
+          image: project2Image,
           link: 'https://example.com/project-one', // Add this line
           technologies: ['React', 'CSS', 'JavaScript'], // We'll use this later
         },
